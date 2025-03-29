345d9de0c8c212a28ab14542e2cfc0a98798d4b4
suef606 <sufe3005@gmail.com>
2025-03-29 20:51:58 +0900

    Part 4-1 Confirmation URL 회원가입

--- components/auth/signup.tsx
+++ components/auth/signup.tsx
@@ -1,13 +1,40 @@
+"use client";
+
 import { Button, Input } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function SignUp({ setView }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmationRequired, setConfirmationRequired] = useState(false);
+
+  const supabase = createBrowserSupabaseClient();
+  // signup mutation
+  const signupMutation = useMutation({
+    mutationFn: async () => {
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+          emailRedirectTo: "http://localhost:3000/signup/confirm",
+        },
+      });
+
+      if (data) {
+        setConfirmationRequired(true);
+      }
+
+      if (error) {
+        alert(error.message);
+      }
+    },
+  });
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="pt-10 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-lg border border-gray-400 bg-white gap-2 rounded-md">
+      <div className="pt-10 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-lg border border-gray-400 bg-white gap-2">
         <img src={"/images/inflearngram.png"} className="w-60 mb-6" />
         <Input
           value={email}
@@ -25,12 +52,14 @@ export default function SignUp({ setView }) {
         />
         <Button
           onClick={() => {
-            console.log("signup");
+            signupMutation.mutate();
           }}
+          loading={signupMutation.isPending}
+          disabled={confirmationRequired}
           color="light-blue"
           className="w-full text-md py-1"
         >
-          가입하기
+          {confirmationRequired ? "메일함을 확인해주세요" : "가입하기"}
         </Button>
       </div>
 
